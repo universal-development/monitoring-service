81da872e968afb0afb2fb6a5b2340e6c16466dca
Denis O <denis@universal-development.com>
2017-03-27 19:49:36 +0300

    Added h2 db initialization

--- src/main/java/com/unidev/app/servicemonitoring/ServicemonitoringApplication.java
+++ src/main/java/com/unidev/app/servicemonitoring/ServicemonitoringApplication.java
@@ -2,11 +2,22 @@ package com.unidev.app.servicemonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.h2.server.web.WebServlet;
+
 
 @SpringBootApplication
 public class ServicemonitoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicemonitoringApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
 	}
 }
